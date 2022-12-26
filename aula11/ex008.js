9a6a88c010084a46a78bf0027e3552ea60a2884a
Sweyd Manaf <abdulsweyd@gmail.com>
2022-12-26 16:33:58 +0200

    Condições (Parte 1)

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 78
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+
+if (vel > 60){ // Condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
