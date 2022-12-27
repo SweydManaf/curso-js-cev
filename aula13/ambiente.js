7ddf0233259ced9ba834650c82d0fa562a0dffc1
Sweyd Manaf <abdulsweyd@gmail.com>
2022-12-27 16:03:19 +0200

    Repetições (Parte 1)
    Estrutura while e do while

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+/*
+var c = 1 
+while (c <= 10){
+    console.log(`Passo ${c}`)
+    c++ // c += 1
+}
+*/
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
